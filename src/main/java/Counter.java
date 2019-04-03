d1e7fee7391d1c43f91a0a6973b43bbeb924bcc8
Andrey <mr.andreykolodiy@gmail.com>
2019-04-03 21:35:50 +0200

    test

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,2 +1,31 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Counter {
+
+    public static void main(String[] args) throws IOException {
+        //random.org/integers
+        String url = "https://www.random.org/integers/?num=10000&min=0&max=1&col=1&base=10&format=html&rnd=new";
+        Document document = Jsoup.connect(url).get();
+
+        List<String> numbers = new ArrayList<>();
+
+        Elements data = document.getElementsByAttributeValue("class", "data");
+        data.forEach(date ->{
+            String numb = date.text();
+            numbers.add(numb);
+        });
+
+        numbers.forEach(numb ->{
+            numb.replaceAll("\\s+","");
+            System.out.println(numb.length());
+        });
+    }
 }
+
+
