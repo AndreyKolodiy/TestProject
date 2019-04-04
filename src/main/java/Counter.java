98b586c98559a64a1466fca1101a1e089b9ba86b
Andrey <mr.andreykolodiy@gmail.com>
2019-04-04 16:44:19 +0200

    new test

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -4,16 +4,16 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Counter {
 
-    final private static String url = "https://www.random.org/integers/?num=100&min=0&max=1&col=1&base=10&format=html&rnd=new";
+    final private static String url = "https://www.random.org/integers/?num=10000&min=0&max=1&col=1&base=10&format=html&rnd=new";
 
     public static void main(String[] args) throws IOException {
 
         StringBuffer buffer = new StringBuffer();
-//        StringBuffer buffer = new StringBuffer("1234567897897891999999999999999999314157177171");
-
         for (int i = 0; i < 100; i++) {
             Document document = Jsoup.connect(url).get();
             Elements data = document.getElementsByAttributeValue("class", "data");
@@ -21,21 +21,47 @@ public class Counter {
             buffer.append(numb);
         }
 
+        Calc calc = new Calc();
+
+        System.out.println("Обработанно " + buffer.length() + " симфолов из них повторяются : ");
+
+        calc.calculete(buffer, 1, 1);
+        System.out.println();
+        calc.calculete(buffer,2,2);
+        System.out.println();
+        calc.calculete(buffer,3,3);
+
+    }
+}
+
+class Calc {
+
+    public void calculete(StringBuffer buffer, int numbersSize, int interval) {
         List<String> tripleNumbers = new ArrayList<>();
         int start = 0;
-        int end = 2;
+        int end = numbersSize;
 
-        for (int i = 0; i < buffer.length(); i += 2) {
-            tripleNumbers.add(buffer.substring(start, end));
-            start = start + 2;
-            end = end + 2;
+        if (numbersSize % 2 == 0 || numbersSize == 1) {
+            for (int i = 0; i < buffer.length(); i += interval) {
+                tripleNumbers.add(buffer.substring(start, end));
+                start = start + interval;
+                end = end + interval;
+            }
+        } else {
+            for (int i = 0; i < buffer.length() - 1; i += interval) {
+                tripleNumbers.add(buffer.substring(start, end));
+                start = start + interval;
+                end = end + interval;
+            }
         }
 
         Set<String> uniqueTripleNumbers = new HashSet<String>(tripleNumbers);
         for (String word : uniqueTripleNumbers) {
-            System.out.println(word + ": " + (Collections.frequency(tripleNumbers, word) * 100 / tripleNumbers.size()) + " %");
+            System.out.println(word + ": " + Collections.frequency(tripleNumbers, word) + " " + ((Collections.frequency(tripleNumbers, word) * 100 / tripleNumbers.size())) + " %");
         }
     }
+
+
 }
 
 
